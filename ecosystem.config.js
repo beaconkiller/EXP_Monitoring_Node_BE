5e86cf59c1c72f248d2c87992b91c531bf67e1b3
beaconkiller <beaconkiller@gmail.com>
2025-02-28 16:20:58 +0700

    CHORE : Added a new value in eco.js for future work of JWT.

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+// pm2 stop 0 && pm2 start ecosystem.config.js --env dev && pm2 logs 0
+
+module.exports = {
+  apps: [{
+      name: "TF_EAppr_BE",
+      script: 'index.js',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      ignore_watch: ["node_modules", "file_storage"],
+      env:{
+        MAIL_ID: "eapprovaltf@gmail.com",
+        MAIL_PW: "pdsa tsvn hemx zgqb",
+        LOGS_FORMAT: "dev",
+        JWT_SECRET: "secret",
+        THE_KEY_V1 : 'NGASAL',
+      },
+      env_dev: {
+          PORT: 3026,
+          NODE_ENV: "dev",
+          E_APPROVE_HOST: '192.168.18.4',
+          E_APPROVE_USER: 'itdev',
+          E_APPROVE_PWD: 'bima123456',
+      },
+      env_prod: {
+          PORT: 3026,
+          NODE_ENV: "prod",
+          E_APPROVE_HOST: 'localhost',
+          E_APPROVE_USER: 'itdev',
+          E_APPROVE_PWD: 'bima123456',
+      },
+  }],
+};
