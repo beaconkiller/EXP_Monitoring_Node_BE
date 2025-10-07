087476b35fcd24852f36d492e8bebcbef49c9262
beaconkiller <beaconkiller@gmail.com>
2025-10-07 15:13:29 +0700

    quicksave : test script

---
+++ js_scripts/script_save.js
@@ -0,0 +1,73 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+
+
+
+let config = {
+    "data": [
+        {
+            "path": "EXP_Monitoring_BE",
+            "branch": "master",
+        },
+        {
+            "path": "EXP_Monitoring_NG_FE",
+            "branch": "master",
+        }
+    ],
+    "key": ""
+}
+
+
+
+async function main() {
+    const args = process.argv.slice(2);
+
+    if (args.length == 0) {
+        console.log(' ');
+        console.log('=============================')
+        console.log('- Arguments cannot be empty -')
+        console.log('=============================')
+        console.log(' ');
+        return;
+    }
+
+    try {
+        baseDir = path.join(
+            __dirname,  // JS_SCRIPTS FOLDER
+            '..',       // ROOT APP FOLDER
+            '..'        // PARENT ROOT FOLDER
+        )
+
+        for (var el of config['data']) {
+            proj_dir = path.join(baseDir, el['path']);
+            let cmd = ''
+                + 'cd ' + proj_dir+ ' && ' 
+                + 'git add . &&' 
+                + 'git commit -m "quicksave : '+args+'" && '
+                + 'git push origin '+el['branch']
+            ;
+
+
+            let _res = await new Promise((resolve, reject) => {
+                exec(cmd, (err, stdout) => {
+                    if (err) reject(err);
+                    resolve(stdout);
+                })
+            })
+
+            console.log(_res);
+
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+
+
+};
+
+
+main();
+
